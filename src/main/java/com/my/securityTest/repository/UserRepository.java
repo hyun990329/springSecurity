f7ab7d35e7b5fb1d428098249c0c8439fbdfec99
hyun990329 <dlrkdgus1999@gmail.com>
2025-09-03 12:51:58 +0900

    Security Login 완성

--- src/main/java/com/my/securityTest/repository/UserRepository.java
+++ src/main/java/com/my/securityTest/repository/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     // 아이디가 존재하는지 확인하는 쿼리 메서드
     boolean existsByUsername(String username);
+
+    // username 으로 검색한 결과 리턴
+    UserEntity findByUsername(String username);
 }
